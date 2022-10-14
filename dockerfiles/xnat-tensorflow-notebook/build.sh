37e6cc356cb4e73ada0374ed129a6241fa706e40
Andy Lassiter <andrewl@wustl.edu>
2022-10-14 11:05:59 -0500

    Update version to 0.2.0

--- dockerfiles/xnat-tensorflow-notebook/build.sh
+++ dockerfiles/xnat-tensorflow-notebook/build.sh
@@ -5,5 +5,5 @@ export $(xargs < ../../.env)
 
 docker build --build-arg JH_VERSION="$JH_VERSION"\
              -t xnat/tensorflow-notebook:latest \
-             -t xnat/tensorflow-notebook:0.1.1-SNAPSHOT \
+             -t xnat/tensorflow-notebook:0.2.0 \
              .
