9991f4a9589b08e0d0599e7cfdfc221a49c79255
Andy Lassiter <andrewl@wustl.edu>
2022-11-14 08:53:54 -0600

    Version 0.3.0

--- dockerfiles/xnat-monai-notebook/build.sh
+++ dockerfiles/xnat-monai-notebook/build.sh
@@ -4,6 +4,6 @@ set -e
 export $(xargs < ../../.env)
 
 docker build --build-arg JH_VERSION="$JH_VERSION" \
-             -t xnat/monai-notebook:0.3.0-beta1 \
+             -t xnat/monai-notebook:0.3.0 \
              -t xnat/monai-notebook:latest \
              .
