d4bfdd5f773601aa651009d8950ccce1fabbf6cf
Andy Lassiter <andrewl@wustl.edu>
2023-06-27 11:55:58 -0500

    Bump to 1.0.0-RC2

--- dockerfiles/xnat-monai-notebook/build.sh
+++ dockerfiles/xnat-monai-notebook/build.sh
@@ -4,6 +4,6 @@ set -e
 export $(xargs < ../../.env)
 
 docker build --build-arg JH_VERSION="$JH_VERSION" \
-             -t xnat/monai-notebook:1.0.0-RC1 \
+             -t xnat/monai-notebook:1.0.0-RC2 \
              -t xnat/monai-notebook:latest \
              .
