d755258427d1fbf89b23344d4288d06566550819
Andy Lassiter <andrewl@wustl.edu>
2023-12-19 13:45:59 -0600

    Update the datascience notebook with packages for dashboard development.

--- dockerfiles/xnat-datascience-notebook/build.sh
+++ dockerfiles/xnat-datascience-notebook/build.sh
@@ -3,6 +3,6 @@ set -e
 
 export $(xargs < ../../.env)
 
-docker build --build-arg JH_VERSION="$JH_VERSION"\
+docker build --no-cache --build-arg JH_VERSION="$JH_VERSION"\
              -t xnat/datascience-notebook:1.1.0-SNAPSHOT \
              .
